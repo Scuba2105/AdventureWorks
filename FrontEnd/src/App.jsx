696f299a4592707139d3997f357ef92e4f2302a2
Steven Bradbury <scuba2150@gmail.com>
2024-11-19 22:39:52 +1100

    started menu

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,16 +2,28 @@ import { useState } from 'react'
 import './App.css'
 import { Logo } from './components/Logo'
 import { MenuOptions } from './components/MenuOptions'
+import { Dashboard } from './components/dashboard/Dashboard'
 
 function App() {
+
+  const [page, setPage] = useState("Dashboard"); 
+  console.log(page);
+
+  function changePage(e, setPage) {
+    const pageSelected = e.currentTarget.querySelector("label").textContent;
+    setPage(pageSelected);
+  }
+
   return (
     <>
       <div className='app-container'>
         <div className="sidenav flex-c-vert">
           <Logo />
-          <MenuOptions />
+          <MenuOptions setPage={setPage} changePage={changePage}/>
         </div>
-        <div className="main-area">Main Area</div>
+        <div className="main-area">
+          {page=="Dashboard" && <Dashboard />}
+        </div>
       </div>
     </>
   )
--- FrontEnd/src/components/MenuOptions.jsx
+++ FrontEnd/src/components/MenuOptions.jsx
@@ -1,8 +1,8 @@
 import { Dashboard } from "./svg/menu-option-svg"
 
-export function MenuOptions() {
+export function MenuOptions({setPage, changePage}) {
     return (
-        <div className="menu-option flex-c-vert">
+        <div className="menu-option flex-c-vert" onClick={(e) => changePage(e, setPage)}>
             <Dashboard size='35px' color='white' />
             <label>Dashboard</label>
         </div>
